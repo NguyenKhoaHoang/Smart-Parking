c9f5ddebbebd729b9eb7849cb685bfba41d91f22
Nguyễn Khoa Hoàng <blcm2486@gmail.com>
2022-06-01 19:42:28 +0700

    update code phan cung

--- smart-parking/smart-parking.ino
+++ smart-parking/smart-parking.ino
@@ -1,5 +1,7 @@
 //*******************************libraries********************************
 //RFID-----------------------------
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
 #include <SPI.h>
 #include <MFRC522.h>
 #include <SoftwareSerial.h>
@@ -8,19 +10,22 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 //************************************************************************
-#define SS_PIN  D2  //D2
-#define RST_PIN D1  //D1
+#define SS_PIN  D3  //D3
+#define RST_PIN D0  //D0
+const char* device_token  = "8ceb36c810343326";
+LiquidCrystal_I2C lcd(0x27,16,2);
+String number_empty;
+//************************************************************************
+//String URL = "http://192.168.1.19:81/smart-parking/getdata.php";
 #define ServoPort D4 
 Servo myservo1;
 //************************************************************************
 MFRC522 mfrc522(SS_PIN, RST_PIN); // Create MFRC522 instance.
 //************************************************************************
 /* Set these to your desired credentials. */
-const char *ssid = "404 Not Found";
-const char *password = "244466666";
-const char* device_token  = "8ceb36c810343326";
-//************************************************************************
-String URL = "http://192.168.1.114:81//smart-parking/getdata.php"; //computer IP or the server domain
+const char *ssid = "khoahoang";
+const char *password = "hoang135792486"; //computer IP or the server domain
+String URL = "http://smart-parking-pbl5.herokuapp.com/getdata.php"; //computer IP or the server domain
 String getData, Link;
 //********************connect to the WiFi******************
 void connectToWiFi(){
@@ -45,6 +50,11 @@ void connectToWiFi(){
 }
 //************************************************************************
 void setup() {
+  // Print a message to the LCD.
+  Wire.begin(D1,D2);
+  lcd.init();
+  lcd.clear();
+  lcd.backlight();
   myservo1.attach(ServoPort);
   myservo1.write(0);
   //delay(1000);
@@ -54,7 +64,30 @@ void setup() {
   mfrc522.PCD_Init(); // Init MFRC522 card
   //---------------------------------------------
   connectToWiFi();
+  GetCurrentNumber();
 }
+
+void GetCurrentNumber(){
+  Serial.println("Getting number empty place");
+//  String URLNumber = "http://192.168.1.19:81/smart-parking/show_current_number.php";
+  String URLNumber = "http://smart-parking-pbl5.herokuapp.com/show_current_number.php";
+  if(WiFi.isConnected()){
+    WiFiClient client;
+    HTTPClient http;    //Declare object of class HTTPClient
+    
+    //GET methode
+    Link = URLNumber;
+    http.begin(client,Link); //initiate HTTP request   //Specify content-type header
+    int httpCode = http.GET();   //Send the request
+    String payload = http.getString();    //Get the response payload
+    Serial.println(Link);   //Print HTTP return code
+    Serial.println(httpCode);   //Print HTTP return code
+    Serial.println(payload);    //Print request response payload
+    number_empty = payload;
+  }
+  
+}
+
 
 //************send the Card UID to the website*************
 void SendCardID( long Card_uid ){
@@ -67,31 +100,35 @@ void SendCardID( long Card_uid ){
     //GET methode
     Link = URL + getData;
     http.begin(client,Link); //initiate HTTP request   //Specify content-type header
-    
     int httpCode = http.GET();   //Send the request
     String payload = http.getString();    //Get the response payload
-
     Serial.println(Link);   //Print HTTP return code
     Serial.println(httpCode);   //Print HTTP return code
     Serial.println(Card_uid);     //Print Card ID
     Serial.println(payload);    //Print request response payload
 
+  
+    
     if (httpCode == 200) {
       if (payload.substring(0, 5) == "login") {
         String user_name = payload.substring(5);
+        String number = payload.substring(8,9);
         myservo1.write(120);
         delay (2000);
         myservo1.write(0);
     //  Serial.println(user_name);
+        number_empty = number;
+        
 
       }
       else if (payload.substring(0, 6) == "logout") {
         String user_name = payload.substring(6);
+        String number = payload.substring(9,10);
         myservo1.write(120);
         delay (2000);
         myservo1.write(0);
     //  Serial.println(user_name);
-        
+        number_empty = number;
       }
       else if (payload == "succesful") {
 
@@ -106,6 +143,11 @@ void SendCardID( long Card_uid ){
 }
 //************************************************************************
 void loop() {
+  lcd.setCursor(1,0);
+  lcd.print("Smart Parking");
+  lcd.setCursor(2,1);
+  lcd.print("Have slot: ");
+  lcd.print(number_empty);
   //check if there's a connection to Wi-Fi or not
   if(!WiFi.isConnected()){
     connectToWiFi();    //Retry to connect to Wi-Fi
